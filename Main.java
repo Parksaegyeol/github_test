247469a4110861a9f09f5fc003326732f22acf6d
parksaegyeol <dmsgkrkt1@naver.com>
2024-09-05 16:38:36 +0900

    자바 소스 첫커밋

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("깃허브 레포지토리 클로 파일에 Main클래스 추가");
+        int arr[] = new int[4];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i);
+        }
+    }
+}
